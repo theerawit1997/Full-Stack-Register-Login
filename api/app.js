53f44668b7e86b9903238c86d5407d1950e5745e
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2024-03-19 17:33:40 +0700

    Update app.js
    hash password

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ var cors = require("cors");
 var app = express();
 var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 app.use(cors());
 
@@ -15,18 +17,20 @@ const connection = mysql.createConnection({
 });
 
 app.post("/register", jsonParser, function (req, res, next) {
-  connection.execute(
-    "INSERT INTO users (email, password, fname, lname) VALUES (?, ?, ?, ?)",
-    [req.body.email, req.body.password, req.body.fname, req.body.lname],
-    function (err, results, fields) {
-      if (err) {
-        // res.json({ status: "error", message: err });
-        res.json({ status: "error", message: err.message });
-        return;
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    connection.execute(
+      "INSERT INTO users (email, password, fname, lname) VALUES (?, ?, ?, ?)",
+      [req.body.email, hash, req.body.fname, req.body.lname],
+      function (err, results, fields) {
+        if (err) {
+          // res.json({ status: "error", message: err });
+          res.json({ status: "error", message: err.message });
+          return;
+        }
+        res.json({ status: "success" });
       }
-      res.json({ status: "success" });
-    }
-  );
+    );
+  });
 });
 
 app.listen(4000, function () {
