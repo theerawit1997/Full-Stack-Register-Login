c7e994b67e3cab5906f9cc25f584ac09caa2bfa7
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2024-03-20 17:39:34 +0700

    Last update

--- react-login/src/SignInSide.js
+++ react-login/src/SignInSide.js
@@ -154,13 +154,13 @@ export default function SignInSide() {
                 Sign In
               </Button>
               <Grid container>
-                <Grid item xs>
+                {/* <Grid item xs>
                   <Link href="#" variant="body2">
                     Forgot password?
                   </Link>
-                </Grid>
+                </Grid> */}
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="/signup" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
