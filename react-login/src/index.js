a598f1485be0950fccb1425b581eada3fd03fe75
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2024-03-20 15:06:49 +0700

    add templates mui
    Free templates mui

--- react-login/src/index.js
+++ react-login/src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignInSide from "./SignInSide";
+import Pricing from "./Pricing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}></Route>
+      <Route path="/" element={<SignInSide />}></Route>
+      <Route path="/login" element={<SignInSide />}></Route>
+      <Route path="/pricing" element={<Pricing />}></Route>
     </Routes>
   </BrowserRouter>
 );
